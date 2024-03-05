a128fa9614dd7817536ce88dacbc064d57bc2a19
Jonah Köllner <koellnjo@gmail.com>
2024-03-05 16:57:49 +0100

    made ex00 use the algorithm find instead of while

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,20 +6,16 @@
 /*   By: jonahkollner <jonahkollner@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 15:49:17 by jkollner          #+#    #+#             */
-/*   Updated: 2024/03/01 16:30:46 by jonahkollne      ###   ########.fr       */
+/*   Updated: 2024/03/05 16:56:43 by jonahkollne      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <algorithm>
+
 template<typename T>
 typename T::iterator easyfind(T &container, int value) {
-	typename T::iterator it = container.begin();
-	typename T::iterator ite = container.end();
-
-	while (it != ite) {
-		if (*it == value)
-			return it;
-		it++;
-	}
-	return ite;
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	return it; // returns either the found item or .end() to indicate not found
 }
 
